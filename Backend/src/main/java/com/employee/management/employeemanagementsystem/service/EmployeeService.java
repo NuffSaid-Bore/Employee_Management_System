4cde9f4e6359c2a9fedf4cb2d445a2c5fe6ef760
NuffSaid-Bore <borekamohelo@gmail.com>
2024-10-08 15:58:19 +0200

    deleted unused package

--- Backend/src/main/java/com/employee/management/employeemanagementsystem/service/EmployeeService.java
+++ Backend/src/main/java/com/employee/management/employeemanagementsystem/service/EmployeeService.java
@@ -5,8 +5,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.employee.management.employeemanagementsystem.entities.Employee;
 import com.employee.management.employeemanagementsystem.repositories.EmployeeRepository;
