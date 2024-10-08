9a67a2cffbc9ade4fae1dd9ca13c2b8ca5ee266a
NuffSaid-Bore <borekamohelo@gmail.com>
2024-10-08 09:15:00 +0200

    clean up a few server side issues

--- Backend/src/main/java/com/employee/management/employeemanagementsystem/controller/EmployeeController.java
+++ Backend/src/main/java/com/employee/management/employeemanagementsystem/controller/EmployeeController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
